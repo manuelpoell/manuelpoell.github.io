baa1757a17e3d0fdeb8bac10381ef811eb992fb3
Manuel Poell <manuelpoell@gmx.at>
2022-06-12 23:37:17 +0200

    added favicon and meta tags

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -7,9 +7,30 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta
-          name="description"
-          content="Manuel Pöll, Web-Entwickler, Portfolio Homepage"
+        <meta name="description" content="Manuel Pöll, Web-Entwickler" />
+        <meta name="author" content="Manuel Pöll" />
+        <meta property="og:site_name" content="Manuel Pöll" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Manuel Pöll" />
+        <meta property="og:description" content="Manuel Pöll, Web-Entwickler" />
+        <meta property="og:url" content="https://manuelpoell.at/" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/images/favicon-96x96.png"
         />
         <title>Manuel Pöll - Homepage</title>
       </Head>
